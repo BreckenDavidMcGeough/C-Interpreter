17f64938dda04109fae6362b7a817e0b93479f6c
Brecken McGeough <35307107+BreckenDavidMcGeough@users.noreply.github.com>
2023-05-11 15:42:25 -0400

    Update DriverBDM.ml

--- DriverBDM.ml
+++ DriverBDM.ml
@@ -4,20 +4,19 @@
 #use "StorageBDM.ml";;
 
 
-(**code updated with hashtable storage for variables*)
 
 
-let store op stack storage = match op with SL.Const x -> storage#addRval x; stack#push x
-| SL.Var x -> stack#push x
-| SL.Fetch -> let variable = (match (stack#pop) with Some(y) -> y) in let value = storage#lookup variable in (match value with Some(x) -> stack#push x 
-	| None -> print_endline ("Stack Underflow Error: Trying to access '"^variable^"' before initialization"); stack#init [])
-| SL.Neg -> let value = (match stack#pop with Some(a) -> a) in storage#addRval value; stack#push (PolyOps.polyMinus("0",value))
-| SL.Plus ->  (match stack#pop2 with (two,one) -> let result = PolyOps.polyPlus(one,two) in storage#addRval result; stack#push result)
-| SL.Times -> (match stack#pop2 with (two,one) -> let result = PolyOps.polyTimes(one,two) in storage#addRval result; stack#push result)
-| SL.Minus -> (match stack#pop2 with (two,one) -> let result = PolyOps.polyMinus(one,two) in storage#addRval result; stack#push result)
-| SL.Div -> (match stack#pop2 with (two,one) -> let result = PolyOps.polyDiv(one,two) in storage#addRval result; stack#push result)
-| SL.Store -> (match stack#pop2 with (value, variable) -> (match (storage#lookup variable) with Some(x) -> storage#reassign variable value; stack#push value
-	| None -> storage#assign (variable,value); stack#push value))(**bug was caused by mismatched parenthesis*)
+let store op stack storage = match op with SL.Const x -> storage#addRval x; stack#push x; print_endline ("after "^x); stack#printStack
+| SL.Var x -> stack#push x; print_endline ("after "^x); stack#printStack
+| SL.Fetch -> let variable = (match (stack#pop) with Some(y) -> y) in let value = storage#lookup variable in (match value with Some(x) -> stack#push x; print_endline ("after Fetch"); stack#printStack 
+	| None -> print_endline ("Stack Underflow Error: Trying to access '"^variable^"' before initialization"); stack#init []; print_endline ("after Fetch"); stack#printStack)
+| SL.Neg -> let value = (match stack#pop with Some(a) -> a) in storage#addRval value; stack#push (PolyOps.polyMinus("0",value)); print_endline ("after Neg"); stack#printStack
+| SL.Plus ->  (match stack#pop2 with (two,one) -> let result = PolyOps.polyPlus(one,two) in storage#addRval result; stack#push result; print_endline ("after Plus"); stack#printStack)
+| SL.Times -> (match stack#pop2 with (two,one) -> let result = PolyOps.polyTimes(one,two) in storage#addRval result; stack#push result; print_endline ("after Times"); stack#printStack)
+| SL.Minus -> (match stack#pop2 with (two,one) -> let result = PolyOps.polyMinus(one,two) in storage#addRval result; stack#push result; print_endline ("after Minus"); stack#printStack)
+| SL.Div -> (match stack#pop2 with (two,one) -> let result = PolyOps.polyDiv(one,two) in storage#addRval result; stack#push result; print_endline ("after Div"); stack#printStack)
+| SL.Store -> (match stack#pop2 with (value, variable) -> (match (storage#lookup variable) with Some(x) -> storage#reassign variable value; stack#push value; print_endline ("after Store"); stack#printStack
+	| None -> storage#assign (variable,value); stack#push value; print_endline ("after Store"); stack#printStack))(**bug was caused by mismatched parenthesis*)
 | SL.Pop -> ()
 ;;
 
@@ -34,40 +33,29 @@ let compileCLine str storage = let stack = new eval_stack_bdm in let pf = (postf
 
 
 (**one instance of storage created to maintain variable storage redundancy throughout lifetime of program and multiple lines of c code*)
-let processFile filename = let storage = new storage_bdm in let file = open_in filename in try while true do let line = input_line file in compileCLine line storage
-done with End_of_file -> close_in file; storage#printChanges
+let readLines filename =
+  let ic = open_in filename in
+     let tryRead () = try Some (input_line ic) with End_of_file -> None in
+        let rec loop acc = match tryRead () with
+          | Some s -> let line = String.trim(s)
+                      in if (line = "" || line = "int main(){" || line = "}" || line = "return 0;" || line = "#include <stdio.h>") then loop acc     (*skip blank lines*)
+                                      else loop (acc @ [line])
+          | None -> close_in ic; acc
+        in loop []
+;;
+(**readLines function from stack overflow: https://stackoverflow.com/questions/5774934/how-do-i-read-in-lines-from-a-text-file-in-ocaml*)
+
+
+let rec processFile lineslist storage = match lineslist with [] -> storage#printChanges 
+| line::rest -> compileCLine line storage; processFile rest storage
+;;
+
+let processor filename = let lines = readLines filename in let s = new storage_bdm in processFile lines s
 ;;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-(**let store op stack storage = match op with SL.Const x -> stack#push x
-| SL.Var x -> stack#push x
-| SL.Fetch -> let variable = (match (stack#pop) with Some(y) -> y) in let value = storage#lookup variable in (match value with Some(x) -> stack#push x 
-	| None -> print_endline ("Error: Trying to acces '"^variable^"' before initialization"); stack#init [])
-| SL.Neg -> let value = (match stack#pop with Some(a) -> a) in stack#push (PolyOps.polyMinus("0",value))
-| SL.Plus -> let valuetwo = (match stack#pop with Some(a) -> a) in let valueone = (match stack#pop with Some(b) -> b) in stack#push (PolyOps.polyPlus(valueone,valuetwo))
-| SL.Times -> let valuetwo = (match stack#pop with Some(a) -> a) in let valueone = (match stack#pop with Some(b) -> b) in stack#push (PolyOps.polyTimes(valueone,valuetwo))
-| SL.Minus -> let valuetwo = (match stack#pop with Some(a) -> a) in let valueone = (match stack#pop with Some(b) -> b) in stack#push (PolyOps.polyMinus(valueone,valuetwo))
-| SL.Div -> let valuetwo = (match stack#pop with Some(a) -> a) in let valueone = (match stack#pop with Some(b) -> b) in stack#push (PolyOps.polyDiv(valueone,valuetwo))
-| SL.Store -> let value = (match stack#pop with Some(a) -> a) in let variable = (match stack#pop with Some(b) -> b) 
-	in (match (storage#lookup variable) with Some(x) -> storage#reassign variable value; stack#push value
-	| None -> storage#assign (variable,value); stack#push value) (**bug was caused by mismatched parenthesis*)
-| SL.Pop -> ()
-;;*)
+(**processor "your_test_file.c";;*)
 
 
 
